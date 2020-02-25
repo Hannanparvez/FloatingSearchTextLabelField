bf3871304a9e6a735fdceff362761f5a8fcdc093
hannan <darhannan5@gmail.com>
2020-02-25 17:02:30 +0530

    Initial commit

---
+++ FloatingSearchTextLabelField/FloatingSearchTextLabelField.h
@@ -0,0 +1,19 @@
+//
+//  FloatingSearchTextLabelField.h
+//  FloatingSearchTextLabelField
+//
+//  Created by hannan parvez on 25/02/20.
+//  Copyright © 2020 hannan parvez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FloatingSearchTextLabelField.
+FOUNDATION_EXPORT double FloatingSearchTextLabelFieldVersionNumber;
+
+//! Project version string for FloatingSearchTextLabelField.
+FOUNDATION_EXPORT const unsigned char FloatingSearchTextLabelFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FloatingSearchTextLabelField/PublicHeader.h>
+
+
